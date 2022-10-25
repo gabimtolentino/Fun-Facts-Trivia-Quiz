d2868f599097dca7b12a3c069a7b6d66ddcf9482
gabimtolentino <gabimonica.t@gmail.com>
2022-10-24 23:30:01 -0700

    added timer countdown in javascript

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,11 @@ var correct = document.querySelector(".correct");
 var incorrect = document.querySelector(".incorrect");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
+var timerEl = document.getElementById('countdown');
+
+function countdown() {
+}
+    var timeLeft = 20;
 
 function startGame() {
     timerCount = 20;
